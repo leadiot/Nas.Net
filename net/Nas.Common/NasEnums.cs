57e19c4b263c67644cf53a916472181019354328
Aven <avenyao@outlook.com>
2025-12-26 14:00:57 +0800

    枚举说明完善。

--- net/Nas.Common/NasEnums.cs
+++ net/Nas.Common/NasEnums.cs
@@ -1,4 +1,6 @@
-﻿namespace Com.Scm.Nas
+﻿using System.ComponentModel;
+
+namespace Com.Scm.Nas
 {
     /// <summary>
     /// 文件类型
@@ -9,10 +11,12 @@
         /// <summary>
         /// 目录
         /// </summary>
+        [Description("目录")]
         Dir,
         /// <summary>
         /// 文档
         /// </summary>
+        [Description("文档")]
         Doc
     }
 
@@ -25,22 +29,27 @@
         /// <summary>
         /// 全量上传
         /// </summary>
+        [Description("全量上传")]
         AllUpload,
         /// <summary>
         /// 增量上传
         /// </summary>
+        [Description("增量上传")]
         IncUpload,
         /// <summary>
         /// 双向同步
         /// </summary>
+        [Description("双向同步")]
         Sync,
         /// <summary>
         /// 全量下载
         /// </summary>
+        [Description("全量下载")]
         AllDownload,
         /// <summary>
         /// 增量下载
         /// </summary>
+        [Description("增量下载")]
         IncDownload
     }
 
@@ -53,54 +62,67 @@
         /// <summary>
         /// 删除
         /// </summary>
+        [Description("删除")]
         Delete,
         /// <summary>
         /// 创建
         /// </summary>
+        [Description("创建")]
         Create,
         /// <summary>
         /// 更名
         /// </summary>
+        [Description("更名")]
         Rename,
         /// <summary>
         /// 修改
         /// </summary>
+        [Description("修改")]
         Change,
         /// <summary>
         /// 移动
         /// </summary>
+        [Description("移动")]
         Move,
         /// <summary>
         /// 复制
         /// </summary>
+        [Description("复制")]
         Copy,
         /// <summary>
         /// 压缩
         /// </summary>
+        [Description("压缩")]
         Compress,
         /// <summary>
         /// 解压
         /// </summary>
+        [Description("解压")]
         Decompress,
         /// <summary>
         /// 分享
         /// </summary>
+        [Description("分享")]
         Share,
         /// <summary>
         /// 解除分享
         /// </summary>
+        [Description("解除分享")]
         Unshare,
         /// <summary>
         /// 隐私
         /// </summary>
+        [Description("隐私")]
         Lock,
         /// <summary>
         /// 解除隐私
         /// </summary>
+        [Description("解除隐私")]
         Unlock,
         /// <summary>
         /// 恢复
         /// </summary>
+        [Description("恢复")]
         Restore,
     }
 
@@ -113,14 +135,17 @@
         /// <summary>
         /// 进行中
         /// </summary>
+        [Description("进行中")]
         Running,
         /// <summary>
         /// 暂停中
         /// </summary>
+        [Description("暂停中")]
         Suspend,
         /// <summary>
         /// 已中止
         /// </summary>
+        [Description("已中止")]
         Stoped
     }
 }
